51462b19c731923f515609373dc2117f858b371d
Eren Küçük <43830824+neetkiss@users.noreply.github.com>
2023-10-07 18:38:02 +0300

    v1.0

---
+++ src/trendyol/Marketplace/TrendyolMarketplace.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Neetkiss\Trendyol\Marketplace;
+
+use Neetkiss\Trendyol\TrendyolRequest;
+
+class TrendyolMarketplace
+{
+    public $supplierId;
+    public $username;
+    public $password;
+
+    function __construct($supplierId = null, $username = null, $password = null){
+        $this->supplierId = $supplierId;
+        $this->username   = $username;
+        $this->password   = $password;
+    }
+
+    public function TrendyolRequest(){
+        return new TrendyolRequest($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceCategories(){
+        return new TrendyolMarketplaceCategories($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceProducts(){
+        return new TrendyolMarketplaceProducts($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceBrands(){
+        return new TrendyolMarketplaceBrands($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceShipment(){
+        return new TrendyolMarketplaceShipment($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceAddresses(){
+        return new TrendyolMarketplaceAddresses($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceOrders(){
+        return new TrendyolMarketplaceOrders($this->supplierId, $this->username, $this->password);
+    }
+
+    public function TrendyolMarketplaceCustomerQuestions(){
+        return new TrendyolMarketplaceCustomerQuestions($this->supplierId, $this->username, $this->password);
+    }
+}
---
+++ src/trendyol/Marketplace/TrendyolMarketplaceCategories.php
@@ -0,0 +1,98 @@
+<?php
+
+    namespace Neetkiss\Trendyol\Marketplace;
+
+    use Neetkiss\Trendyol\TrendyolRequest;
+
+    class TrendyolMarketplaceCategories{
+
+        public $supplierId;
+        public $username;
+        public $password;
+
+        function __construct($supplierId = null, $username = null, $password = null){
+            $this->supplierId = $supplierId;
+            $this->username   = $username;
+            $this->password   = $password;
+        }
+
+        public function request(){
+            return new TrendyolRequest($this->supplierId, $this->username, $this->password);
+        }
+
+        public function get_categories(){
+            $url    = 'https://api.trendyol.com/sapigw/product-categories';
+            $result = $this->request()->get($url);
+            return $result;
+        }
+
+        public function get_my_categories(){
+            $url    = 'https://www.trendyol.com/sr?mid='.$this->supplierId;
+            $result = file_get_contents($url);
+            preg_match_all('/{"id":"\d+","text":"([^"]+)","beautifiedName":"[^"]+","count":([^"]+),"filtered":false,"filterField":"(webCategoryIds|leafCategoryIds)","type":"(WebCategory|LeafCategory)","url":"[^"]+"}/', $result, $matches);
+
+            $supplider_cats = [];
+            $total_product  = 0;
+            foreach($matches[1] as $id => $match){
+
+                $category_info_json = json_decode(file_get_contents((__DIR__) . '/../assets/category_info.json'), true);
+                $keys               = $this->trendyol_array_search($category_info_json['Categories'], 'Name', trim($match));
+
+                $supplider_cats[] = [
+                    'cat_id'   => trim($keys['Id']),
+                    'cat_name' => trim($match),
+                    'count'    => $matches[2][$id]
+                ];
+
+                $total_product += $matches[2][$id] ?? 0;
+            }
+
+            $supplider_cats['total_product_count'] = $total_product;
+
+            return $supplider_cats;
+        }
+
+        public function get_category_info($category_id = null){
+            $category_info_json = json_decode(file_get_contents((__DIR__) . '/../assets/category_info.json'), true);
+            $keys               = $this->trendyol_array_search($category_info_json['Categories'], 'Id', $category_id);
+            $url                = 'https://api.trendyol.com/sapigw/product-categories/'.$category_id.'/attributes';
+            $result             = $this->request()->get($url);
+            $result->commission = $keys['Commission'] ?? 0;
+            return $result;
+        }
+
+        public $trendyol_array_search_result = null;
+
+        public function trendyol_array_search($data = [], $key = null, $value = null){
+            if(isset($data) and $data != null){
+                foreach($data as $category){
+
+                    if(isset($category[$key]) and ($category[$key] == $value or $this->calculateSimilarityScore($category[$key], $value) > 80)){
+                        $this->trendyol_array_search_result = $category;
+                    }
+
+                    if(isset($category['Nodes']) and $category['Nodes'] != null){
+                        $this->trendyol_array_search($category['Nodes'], $key, $value);
+                    }
+
+                }
+            }else{
+                return 2;
+            }
+            return $this->trendyol_array_search_result;
+        }
+
+        private function calculateSimilarityScore($string1, $string2){
+
+            $string1 = str_replace(['\u0026', '&'], ['&', ' ve '], htmlspecialchars_decode($string1));
+            $string2 = str_replace(['\u0026', '&'], ['&', ' ve '], htmlspecialchars_decode($string2));
+
+            $levenshteinDistance = levenshtein($string1, $string2);
+            $maxLength           = max(strlen($string1), strlen($string2));
+
+            $similarityScore = 100 * (1 - $levenshteinDistance / $maxLength);
+
+            return $similarityScore;
+        }
+
+    }
